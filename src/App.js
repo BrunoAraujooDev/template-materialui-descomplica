55e61f1b262b14e109e12f21e911417d92c8a87b
Bruno Araujo <brunoaraujo2395@gmail.com>
2023-11-12 20:05:58 -0300

    criando funcionalidade de filtrar a lista

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa.tsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
